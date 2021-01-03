e8f816b43bf39fc74a8a5441637b4cafe0a06c46
dasistdaniel <d.intrup@googlemail.com>
2021-01-03 09:30:53 +0100

    added counter

--- baseSpiders/br.py
+++ baseSpiders/br.py
@@ -4,16 +4,17 @@ class brSpider(baseSpiders.baseSpider.baseSpider):
     def get_loop(self, response):
         return response.css('dd.audio')
     
-    def get_date(self, response, item):
+    def get_date(self, response, item, counter):
         return response.css('li.playlist_navi_head::text').extract_first().strip()[-10:]
     
-    def get_time(self, response, item):
-        #return  item.css('ul').extract()
-        return ""
+    def get_time(self, response, item, counter):
+        print (counter)
+        #return response.css("dt.time:nth-child(" + counter + ")").extract()
+        return "yaaa"
 
-    def get_artist(self, response, item):
+    def get_artist(self, response, item, counter):
         return item.css('ul > li > span:nth-child(1)::text').extract_first().strip().lower()
 
-    def get_title(self, response, item):
+    def get_title(self, response, item, counter):
         return item.css('ul > li > span:nth-child(2)::text').extract_first().strip().lower()
 
