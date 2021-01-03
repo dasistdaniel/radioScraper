4d60309bb856766d1649ea56defec9cccc796a7a
dasistdaniel <d.intrup@googlemail.com>
2021-01-03 11:28:26 +0100

    added br stations

--- baseSpiders/br.py
+++ baseSpiders/br.py
@@ -8,13 +8,11 @@ class brSpider(baseSpiders.baseSpider.baseSpider):
         return response.css('li.playlist_navi_head::text').extract_first().strip()[-10:]
     
     def get_time(self, response, item, counter):
-        print (counter)
-        #return response.css("dt.time:nth-child(" + counter + ")").extract()
-        return "yaaa"
+        css = f"dt.time:nth-child({1 + counter * 2})::text"
+        return response.css(css).extract_first() + ":00"
 
     def get_artist(self, response, item, counter):
         return item.css('ul > li > span:nth-child(1)::text').extract_first().strip().lower()
 
     def get_title(self, response, item, counter):
         return item.css('ul > li > span:nth-child(2)::text').extract_first().strip().lower()
-
