88f0ffd8396883d0f3dddd2b3e2fe99ea697e9bb
dasistdaniel <d.intrup@googlemail.com>
2021-01-20 21:58:49 +0100

    fixed bremen 1

--- radioscraper/spiders/rb-bremeneins.py
+++ radioscraper/spiders/rb-bremeneins.py
@@ -1,8 +1,21 @@
-import datetime
 import radioscraper.spiders.baseSpiders.baseRB as network
 
 class stationSpider(network.networkRBSpider):
-    d = datetime.datetime.now()
     name = "rb-bremen1"
     station = "Bremen Eins"
-    start_urls = [f'https://www.radiobremen.de/bremeneins/musik/titelsuche/?wrapurl=%2Fbremeneins%2Fmusik%2Ftitelsuche%2F&selectdate={d.strftime("%Y-%m-%d")}&stunde={d.hour:02}&minute={int(d.minute / 15) * 15:02}']
+    start_urls = ['https://www.bremeneins.de/suche/titelsuche-112.html']
+
+    def get_loop(self, response):
+        return response.css('tr')
+
+    def get_date(self, response, item, counter):
+        return "dea"
+
+    def get_time(self, response, item, counter):
+        return item.css("td:nth-child(1) > div > span::text").extract_first()
+
+    def get_artist(self, response, item, counter):
+        return item.css('td:nth-child(2) > div::text').extract_first()
+
+    def get_title(self, response, item, counter):
+        return item.css('td:nth-child(3) > div::text').extract_first()    
