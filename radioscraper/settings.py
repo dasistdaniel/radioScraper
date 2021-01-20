7273ac91462af270977e0d77c45a9ecaab1337be
dasistdaniel <d.intrup@googlemail.com>
2021-01-20 21:33:10 +0100

    User Agent geändert

--- radioscraper/settings.py
+++ radioscraper/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'radioscraper.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'radioscraper (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
